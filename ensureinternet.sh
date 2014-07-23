19855c4852330acb4d6b3f9f567c6131208d59e0
Johan Rylander <johan@rylander.cc>
2014-07-23 10:41:45 +0200

    Initial

---
+++ ensureinternet.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+
+setConnected() {
+  if (ping -n -W 2 -c 1 130.237.222.80 || ping -n -W 2 -c 1 8.8.8.8 || ping -n -W 2 -c 1 139.130.4.5) 1>&2 > /dev/null; then
+    connected=true
+  else
+    connected=false
+  fi
+}
+
+setConnected
+attempts=0
+while ! ${connected} && [ ${attempts} -lt 5 ] ; do
+  setConnected
+  sleep 5
+  attempts=$((attempts + 1))
+done
+
+if ! ${connected}; then
+  echo "Resetting power to router on `date`"
+  if tdtool --off Router|grep 'TellStick not found'
+  then
+    echo "Rebooting  on `date`"
+    reboot
+  fi
+  sleep 30
+  if tdtool --on Router|grep 'TellStick not found'
+  then
+    echo "Rebooting on `date`"
+    reboot
+  fi
+fi
